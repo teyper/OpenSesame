48b33f853ed42fd839cc709f51aefe9a68639e93
teyper <teyjhanabartonneal@spelman.edu>
2025-03-05 16:03:45 -0500

    creating coin

---
+++ Assets/star/spin.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spin : MonoBehaviour
+{
+    [SerializeField] float speed = 360f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(speed * Time.deltaTime * Vector3.forward);
+    }
+}
