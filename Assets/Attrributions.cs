4b46a6bd33aa49324312e910f7faea4109daa0f1
teyper <teyjhanabartonneal@spelman.edu>
2025-05-05 20:23:35 -0400

    adding assets

--- Assets/Attrributions.cs
+++ Assets/Attrributions.cs
@@ -7,7 +7,11 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        /*
+         * 3D Model and Star: sketchfab.com 
+         * Sky box and third person controlle: unity Asset store 
+         * 
+         */
     }
 
     // Update is called once per frame
